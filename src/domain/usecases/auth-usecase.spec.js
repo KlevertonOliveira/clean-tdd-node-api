87c115cddafaa536a319cb41350c2c4ff7a14480
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-09 11:44:12 -0300

    feat: ensure AuthUseCase calls getUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -2,9 +2,13 @@ import { describe, expect, it } from 'vitest';
 import { MissingParamError } from '../../utils/errors';
 
 class AuthUseCase {
+  constructor(getUserByEmailRepository) {
+    this.getUserByEmailRepository = getUserByEmailRepository;
+  }
   async auth(email, password) {
     if (!email) throw new MissingParamError('email');
     if (!password) throw new MissingParamError('password');
+    await this.getUserByEmailRepository.get(email);
   }
 }
 
@@ -19,5 +23,17 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase();
     const promise = sut.auth('any_email@test.com');
     expect(promise).rejects.toThrow(new MissingParamError('password'));
+  });
+
+  it('Should call getUserByEmailRepository with correct email', async () => {
+    class GetUserByEmailRepositorySpy {
+      async get(email) {
+        this.email = email;
+      }
+    }
+    const getUserByEmailRepositorySpy = new GetUserByEmailRepositorySpy();
+    const sut = new AuthUseCase(getUserByEmailRepositorySpy);
+    await sut.auth('any_email@test.com', 'any_password');
+    expect(getUserByEmailRepositorySpy.email).toBe('any_email@test.com');
   });
 });
