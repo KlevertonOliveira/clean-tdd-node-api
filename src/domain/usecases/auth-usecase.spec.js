a7682c51be0a966c60a460ee8f575182eb9fbe20
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-09 11:00:58 -0300

    feat: ensure AuthUseCase throws if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+
+class AuthUseCase {
+  async auth(email) {
+    if (!email) throw new Error();
+  }
+}
+
+describe('Auth UseCase', () => {
+  it('Should throw if no email is provided', async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth();
+    expect(promise).rejects.toThrow();
+  });
+});
