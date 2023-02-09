ef8837401df119ddd1ebad7b8fdd9b65533edde8
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-09 11:13:23 -0300

    refactor: ensure AuthUseCase throws MissingParamError if no email is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest';
-
+import { MissingParamError } from '../../utils/errors';
 class AuthUseCase {
   async auth(email) {
-    if (!email) throw new Error();
+    if (!email) throw new MissingParamError('email');
   }
 }
 
@@ -10,6 +10,6 @@ describe('Auth UseCase', () => {
   it('Should return null if no email is provided', async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
-    expect(promise).rejects.toThrow();
+    expect(promise).rejects.toThrow(new MissingParamError('email'));
   });
 });
