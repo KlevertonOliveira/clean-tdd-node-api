fad182b917beca5f513e8c1256c6a0a65fdffa48
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-10 13:11:42 -0300

    refactor: update EmailValidator production class to reflect changes made after tests

--- src/utils/helpers/email-validator.js
+++ src/utils/helpers/email-validator.js
@@ -1,6 +1,7 @@
 import validator from 'validator';
 export class EmailValidator {
   isValid(email) {
+    if (!email) throw new MissingParamError('email');
     return validator.isEmail(email);
   }
 }
--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -1,7 +1,11 @@
 import bcrypt from 'bcrypt';
+import { MissingParamError } from '../errors';
 
 export class Encrypter {
   async compare(value, hash) {
+    if (!value) throw new MissingParamError('value');
+    if (!hash) throw new MissingParamError('hash');
+
     const isPasswordValid = await bcrypt.compare(value, hash);
     return isPasswordValid;
   }
