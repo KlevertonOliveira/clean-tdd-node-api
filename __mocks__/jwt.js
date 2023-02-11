55ec58c01dbc72d7d524c9f367bbb63138e59bc8
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-11 12:08:28 -0300

    feat: ensure TokenGenerator returns a token if JWT returns token

---
+++ __mocks__/jwt.js
@@ -0,0 +1,6 @@
+export const jwt = {
+  token: 'any_token',
+  sign(id, secret) {
+    return this.token;
+  },
+};
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,15 +1,22 @@
 import { describe, it, expect } from 'vitest';
-
+import { jwt } from '../../../__mocks__/jwt';
 class TokenGenerator {
   async generate(id) {
-    return null;
+    return jwt.sign(id, 'secret');
   }
 }
 
 describe('Token Generator', () => {
   it('Should return null if JWT returns null', async () => {
     const sut = new TokenGenerator();
+    jwt.token = null;
     const token = await sut.generate('any_id');
     expect(token).toBeNull();
+  });
+
+  it('Should return a token if JWT returns token', async () => {
+    const sut = new TokenGenerator();
+    const token = await sut.generate('any_id');
+    expect(token).toBe(jwt.token);
   });
 });
