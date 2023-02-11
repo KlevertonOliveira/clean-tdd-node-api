c8f43c39c184a28d78bd399314a5388b1b4ccef5
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-02-11 12:31:30 -0300

    feat: create TokenGenerator production class in separate file

---
+++ src/utils/helpers/token-generator.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import { MissingParamError } from '../errors';
+
+export class TokenGenerator {
+  constructor(secret) {
+    this.secret = secret;
+  }
+
+  async generate(id) {
+    if (!this.secret) throw new MissingParamError('secret');
+    if (!id) throw new MissingParamError('id');
+
+    return jwt.sign(id, this.secret);
+  }
+}
